d606f163510ceb388cebf24d161cfc690a09c399
StefanYousif <102787518+StefanYousif@users.noreply.github.com>
2024-06-08 20:36:29 +0200

    src/images

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,5 @@
 import React from "react";
-import deleteIcon from "/public/Images/delet.png"
+import deleteIcon from "../Images/delet.png"
 import starIcon from "../Images/star.png";
 import { array } from "prop-types";
 
